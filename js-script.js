0cbf8453ca18843bb429fbd0689a252b4c53dad4
“Manish” <manishpaulsimon@gmail.comgit config --global user.name Your>
2024-03-26 17:24:46 +0100

    Resize working correctly
    Need to figure out how to provide a default statement if user selects a value more than 100

--- js-script.js
+++ js-script.js
@@ -9,11 +9,18 @@ button.style.justifyContent = 'center';
 button.style.alignItems = 'center';
 button.style.margin = '50px';
 const resize = button.addEventListener('click',function() {
-        let e = prompt('Enter resize value');
-        if (e>100) {
-            return;
-        }
+    mainDiv.removeChild(container);
+    let e = prompt('Enter resize value');
+    if (e>100) {
+        boxGrid(Number(16));
+        mainDiv.appendChild(container);
+        addEventListenerToGrid();
+    }
+    else {
         boxGrid(Number(e));
+        mainDiv.appendChild(container);
+        addEventListenerToGrid();
+    }
 });
 
 
@@ -23,9 +30,31 @@ container.style.display = 'flex';
 container.style.justifyContent = 'center';
 container.style.alignItems = 'center';
 
+//Function to create new Event Listeners
+function addEventListenerToGrid() {
+    const divHover = document.querySelectorAll('.divBoxSingle');
+    divHover.forEach(divBox => {
+        divBox.addEventListener('mouseover',hoverHandler);
+    });
+}
+
+//Function to remove old Event Listners
+function removeEventListenerFromGrid() {
+    const divHover = document.querySelectorAll('.divBoxSingle');
+    divHover.forEach(divBox => {
+        divBox.removeEventListener('mouseover',hoverHandler);
+    }); 
+}
+
+
+
 
 //Create the Div Boxes inside a function
 function boxGrid(gridNumber) {
+
+    removeEventListenerFromGrid();
+    container.innerHTML = '';
+    
     for (let i=0;i<gridNumber;i++) {
             const row = document.createElement('div');
             container.appendChild(row);
@@ -40,15 +69,11 @@ function boxGrid(gridNumber) {
                 row.appendChild(divBox);
             }
     }
-};
-boxGrid(10);
 
-const divHover = document.querySelectorAll('.divBoxSingle');
-divHover.forEach(divBox => {
-    divBox.addEventListener('mouseover',function() {
-        divBox.style.backgroundColor = 'blue';
-    });   
-});
+};
+boxGrid(16);
+addEventListenerToGrid();
+
 
 //Arrange Items in Center
 const mainDiv = document.createElement('div');
@@ -59,6 +84,11 @@ mainDiv.style.alignItems = 'center';
 mainDiv.appendChild(button);
 mainDiv.appendChild(container);
 document.body.appendChild(mainDiv);
+
+//change color of hover
+function hoverHandler() {
+    this.style.backgroundColor = 'blue';
+}
 
 
 
